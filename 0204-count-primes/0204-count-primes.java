d4a411580758c01b1aad969d6aa3ed42e73fbcc6
JANVI <cjanvi1010@gmail.com>
2024-09-12 17:46:00 +0530

    Update 0204-count-primes.java

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,12 +1,17 @@
 class Solution {
     public int countPrimes(int n) {
-    
+     // Sieve of Eratosthenes
+    // Time Complexity O(N * log(logN))
+    // Space Complexity O(N)
         if (n <= 2) return 0;
         
         boolean[] isPrime = new boolean[n];
+        // By Default all the values in the Boolean Array are false
         for (int i = 2; i * i < n; i++) {
+             // False in Array Means Number is Prime
             if (!isPrime[i]) {
                 for (int j = i * i; j < n; j += i) {
+                     // Make all Multiples of i to True
                     isPrime[j] = true;
                 }
             }
