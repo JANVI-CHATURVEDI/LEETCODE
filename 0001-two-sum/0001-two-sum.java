ca94ce32b206555f603ad637acd2292abfcc9932
JANVI <cjanvi1010@gmail.com>
2024-11-06 22:12:04 +0530

    Time: 45 ms (31.67%), Space: 45 MB (19.07%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,15 +1,19 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-    int n = nums.length;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j < n; j++) {
+        int[] result = new int[2];
+        
+        // Loop until the second-last element
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
+                    result[0] = i;
+                    result[1] = j;
+                    return result; // Return immediately when the pair is found
                 }
             }
         }
-        return new int[]{}; // No solution found    
         
-        
+        // Return empty array if no solution is found
+        return new int[0];
     }
 }
