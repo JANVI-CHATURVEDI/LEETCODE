5900c88327a6628248c395289de13094f61d5f8b
JANVI <cjanvi1010@gmail.com>
2024-12-04 12:10:13 +0530

    Time: 3 ms (99.01%), Space: 42.7 MB (88.71%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,21 +1,41 @@
 class Solution {
     public String reverseWords(String s) {
-        String[] words = s.trim().split("\\s+");
+        char[] chars = s.trim().toCharArray();
+        int n = chars.length;
 
-        // Reverse the order of words
-        reverseArray(words);
+        // Reverse the entire string
+        reverse(chars, 0, n - 1);
 
-        return String.join(" ", words);
+        // Reverse each word and handle multiple consecutive spaces
+        int start = 0, end = 0;
+        for (int i = 0; i <= n; i++) {
+            if (i == n || chars[i] == ' ') {
+                reverse(chars, start, end - 1);
+                start = i + 1;
+                end = start;
+            } else {
+                end++;
+            }
+        }
+
+        // Remove extra spaces
+        int writeIndex = 0;
+        for (int i = 0; i < n; i++) {
+            if (i == 0 || chars[i] != ' ' || chars[i - 1] != ' ') {
+                chars[writeIndex++] = chars[i];
+            }
+        }
+
+        return new String(chars, 0, writeIndex);
     }
 
-    private void reverseArray(String[] arr) {
-        int left = 0, right = arr.length - 1;
-        while (left < right) {
-            String temp = arr[left];
-            arr[left] = arr[right];
-            arr[right] = temp;
-            left++;
-            right--;
+    private void reverse(char[] chars, int start, int end) {
+        while (start < end) {
+            char temp = chars[start];
+            chars[start] = chars[end];
+            chars[end] = temp;
+            start++;
+            end--; 
         }
     }
 }
