46dd154e125d463b1cabd90980745e5b5405665f
JANVI <cjanvi1010@gmail.com>
2025-03-06 16:35:23 +0530

    Time: 0 ms (100%), Space: 43.3 MB (21.18%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,47 +1,44 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-     int n= nums.length; int pivotidx=-1;
-      
-     for (int i = n - 2; i >= 0; i--) {
-    if (nums[i] < nums[i + 1]) {
-        pivotidx = i;  // Track the index where nums[i] < nums[i + 1]
-        break;          // We only need the first such pair, so break after finding it
-    }
+    int n= nums.length;int pindx=-1;
 
-}
-
-       if ( pivotidx ==-1){
-          reverse( nums ,0 ,n-1);
-          return;
-        }
-
-
-for (int i = n - 1; i >= 0; i--) { 
-        if(nums[i]>nums[pivotidx]){
-            int temp= nums[pivotidx];
-           nums[pivotidx]=nums[i];
-            nums[i]=temp;
+    for(int i=n-1;i>0;i--){
+        if(nums[i]>nums[i-1]){
+            pindx=i-1;
             break;
+
         }
-}
-           reverse( nums ,pivotidx +1,n-1);
-      }
-      
-      
-     
-      
-    
+ }
 
+ if(pindx==-1){
+    reverse(0,n-1,nums);
+    return;
+   
+ }
 
-    public int[] reverse(int[] nums ,int s ,int e){
-       
-       
-        while(s<e){
-            int temp=nums[s];
-            nums[s]=nums[e];
-            nums[e]=temp;
-             s++;
-            e--;
-        } return nums;
+ for(int i=n-1;i>0;i--){
+    if(nums[i]>nums[pindx]){
+        int temp=nums[pindx];
+ nums[pindx]=nums[i];
+ nums[i]=temp;
+ break;
     }
+ }
+
+reverse(pindx+1,n-1,nums);
+ 
+        
+    }
+
+    public void reverse(int s, int e ,int[]nums){
+        while(s<e){                       
+        int temp=nums[s];
+ nums[s]=nums[e];
+ nums[e]=temp;
+  s++;
+        e--;
+    
+        }
+       
+}
 }
