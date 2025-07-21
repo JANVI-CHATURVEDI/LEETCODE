51551e76fe9fc41e6cd70841d7c59e592303079d
JANVI <cjanvi1010@gmail.com>
2025-07-21 22:16:02 +0530

    Time: 0 ms (100%), Space: 45 MB (9.16%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -10,27 +10,24 @@
  */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null) return head;
 
-        if (head == null || head.next == null) {
-            return head;
-        }
-        
-        ListNode even = head.next;
-        ListNode odd  = head;
-        ListNode evenhead  = even;
+        ListNode odd = head;
+        ListNode even = head.next ;
+        ListNode evenhead = even;
 
-        
-         while (even != null && even.next != null) {
+
+        while( even != null && even.next != null ){
+           odd.next = even.next;
+           odd = odd.next;
+
+           even.next = odd.next;
+           even = even.next;
            
-            odd.next=even.next;
-            odd = odd.next;
+        }
 
-            even.next=odd.next;
-            even = even.next;
-         }
+        odd.next = evenhead;
 
-         odd.next=evenhead;
-
-       return head;
+        return head;
     }
 }
