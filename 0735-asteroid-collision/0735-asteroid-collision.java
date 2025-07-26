2e5489331c52fa13db3664995b775f32623b8403
JANVI <cjanvi1010@gmail.com>
2025-07-26 20:57:22 +0530

    Time: 6 ms (64.44%), Space: 45.6 MB (19.6%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,39 +1,43 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-         Stack<Integer> stack = new Stack<>();
-         
+       
+        Stack<Integer> stack = new Stack <>();
 
-         for(int a: asteroids){
-           boolean alive = true;
+        for(int i=0 ; i< asteroids.length ; i++){
+             boolean alive = true;
+            int a = asteroids[i];
+            
+               while( !stack.isEmpty() && stack.peek() >0 && a < 0 ){
+                int num = -a;
+                int snum = stack.peek();
 
-               while (!stack.isEmpty() && a < 0 && stack.peek() > 0) {
-               int num=-a;
-               if(num>stack.peek()){
-                stack.pop();
-               }
-               else if(num==stack.peek()){
-                 stack.pop();
-                 alive=false;
-                 break;
-               }
-               else{
-                alive=false;
-                break;
-               }
-               }
+                if(num > snum){
+                    stack.pop();
+                }
+                else if(num == snum){
+                    stack.pop(); 
+                    alive = false;
+                    break;
+                }
+                else{
+                    alive = false;
+                    break;
+                }
+             }
 
-               if(alive){
+             if(alive){
                 stack.push(a);
                }
-         }
 
+          }
 
-         int[] result = new int[stack.size()];
+           int[] result = new int[stack.size()];
 
          for (int j = result.length - 1; j >= 0; j--) {
                    result[j] = stack.pop(); // last in → first out
          }
 
          return result;
+
+       }
     }
-}
