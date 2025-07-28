2d2bc7853f65a68d7c4a9cceab19c13604d97af7
JANVI <cjanvi1010@gmail.com>
2025-07-28 21:52:24 +0530

    Time: 8 ms (36.37%), Space: 63.8 MB (57.83%) - LeetHub

--- 2236-maximum-twin-sum-of-a-linked-list/2236-maximum-twin-sum-of-a-linked-list.java
+++ 2236-maximum-twin-sum-of-a-linked-list/2236-maximum-twin-sum-of-a-linked-list.java
@@ -44,7 +44,7 @@ class Solution {
         ListNode slow = head;
         ListNode fast = head;
 
-        while( fast.next.next != null){
+        while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
