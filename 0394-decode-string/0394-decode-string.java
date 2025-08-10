7b60119733a2a9168508d36a2d6aa351dca65ee9
JANVI <cjanvi1010@gmail.com>
2025-08-10 21:56:37 +0530

    Time: 1 ms (69.53%), Space: 41.2 MB (96.64%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,53 +1,57 @@
 class Solution {
     public String decodeString(String s) {
-        int n=s.length(); String result;
-        Stack<String> str = new Stack<>();
-        Stack<Integer> num = new Stack<>();
-        StringBuilder sb = new StringBuilder();
-       
-     
-        for(int i=0;i<n;i++){
-         char ch = s.charAt(i);
-            if(Character.isDigit(ch)){
-                   int numb = 0;
-               while (i < n && Character.isDigit(s.charAt(i))) {
-                  numb = numb * 10 + (s.charAt(i) - '0');
-                   i++;
-               }
-                   i--; // step back because outer loop will also do i++
-                num.push(numb);
+      int n = s.length();
+      Stack<Integer> num = new Stack <>();
+      Stack<String> str = new Stack <>();
+      StringBuilder sb = new StringBuilder();
+
+      for(int i = 0 ; i<n ; i++){
+        char ch = s.charAt(i);
+        
+        if(Character.isDigit(ch)){
+            int numb = 0;
+
+            while(i<n && Character.isDigit(s.charAt(i)) ){
+                numb = numb*10 + (s.charAt(i)- '0');
+                i++;
+            }
+            i--;
+
+            num.push(numb);
+        }
+
+        else{
+            if(ch == '['){
+                String c = String.valueOf(ch);
+                str.push(c);
+            }
+            else if (ch == ']'){
+                while(!str.isEmpty()  &&!str.peek().equals( "[")){
+                    sb.insert(0 , str.pop());
+                }
+                 
+                if(!str.isEmpty() ){
+                    str.pop();
+                }
+                
+                int numb = num.pop();
+                String result = sb.toString().repeat(numb);
+                str.push(result);
+                sb.setLength(0);
             }
             else{
-                if(ch == '['){
-                   String c= String.valueOf(ch);
-                    str.push(c);
-                }
-                else if(ch == ']'){
-                   while (!str.peek().equals("[")) {
-                        sb.insert(0, str.pop());
-                        
-                    }
-                    str.pop();
-                    int numb = num.pop();
-                     result = sb.toString().repeat(numb);
-                    str.push(result);
-                    sb.setLength(0);
-                    
-                }
-                else{
-                   String c= String.valueOf(ch);
-                    str.push(c);
-                }
+                String c = String.valueOf(ch);
+                str.push(c); 
             }
-        }     
-   
-   
-         StringBuilder finalResult = new StringBuilder();
-                   for (String part : str) {
-                         finalResult.append(part);
-                    }
-     
-      return finalResult.toString();
+        }
+      }
 
+      StringBuilder finalresult = new StringBuilder();
+
+      for(String part : str){
+        finalresult.append(part);
+      }
+
+      return finalresult.toString();
     }
 }
