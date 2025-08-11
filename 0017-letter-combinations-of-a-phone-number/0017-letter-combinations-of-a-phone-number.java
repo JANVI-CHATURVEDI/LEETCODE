ebd0ed5fa0f6482c1af4b2b256611c1db69c9cc7
JANVI <cjanvi1010@gmail.com>
2025-08-11 22:04:38 +0530

    Time: 0 ms (100%), Space: 41.9 MB (95.53%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,43 +1,41 @@
 class Solution {
 
     List<String> result = new ArrayList<>();
-
     public List<String> letterCombinations(String digits) {
         int n = digits.length();
-        
-       if(n == 0){
-        return result;
-       }
 
-       Map<Character, String> map = new HashMap<>();
-        map.put('2', "abc");
-        map.put('3', "def");
-        map.put('4', "ghi");
-        map.put('5', "jkl");
-        map.put('6', "mno");
-        map.put('7', "pqrs");
-        map.put('8', "tuv");
-        map.put('9', "wxyz");
+        if(n == 0){
+            return result;
+        }
 
-        backtrack(0, digits, new StringBuilder(), map);
-        return result;
+        Map<Character , String> map = new HashMap<>();
+        map.put('2' , "abc");
+        map.put('3' , "def");
+        map.put('4' , "ghi");
+        map.put('5' , "jkl");
+        map.put('6' , "mno");
+        map.put('7' , "pqrs");
+        map.put('8' , "tuv");
+        map.put('9' , "wxyz");
 
-
+        backtrack(0 , digits , new StringBuilder() , map);
+        return result ; 
     }
 
-    void backtrack(int idx, String digits, StringBuilder temp, Map<Character, String> map) {
-        if (idx == digits.length()) {
+    void backtrack(int idx , String digits , StringBuilder temp , Map<Character , String> map){
+
+        if(idx == digits.length()){
             result.add(temp.toString());
-            return;
+            return ;
         }
 
         char ch = digits.charAt(idx);
         String str = map.get(ch);
 
-        for (int i = 0; i < str.length(); i++) {
+        for(int i=0 ; i<str.length() ; i++){
             temp.append(str.charAt(i));
-            backtrack(idx + 1, digits, temp, map);
-            temp.deleteCharAt(temp.length() - 1); 
+            backtrack(idx+1 , digits , temp , map);
+            temp.deleteCharAt(temp.length()-1);
         }
     }
 }
