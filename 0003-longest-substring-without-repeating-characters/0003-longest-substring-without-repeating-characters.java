a5586637278483c2d05f029f61725008e1ee444f
JANVI <cjanvi1010@gmail.com>
2025-08-12 20:50:47 +0530

    Time: 5 ms (89.33%), Space: 44.6 MB (79.3%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,49 +1,20 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int maxLength = 0;
-        int[] arr = new int[128];  // Frequency array
-        int left = 0;  // Left pointer
-
-        for (int right = 0; right < s.length(); right++) {
-            char c = s.charAt(right);
-            
-            // If duplicate found, move left pointer
-            while (arr[c] > 0) {
-                arr[s.charAt(left)]--;  
-                left++;  
-            }
-
-            arr[c]++;  // Mark character as seen
-            maxLength = Math.max(maxLength, right - left + 1);
-        }
-
-        return maxLength;
-
-       
-       
-       
-       
-       
-       /*   BRUTE FORCE
-       
-       int n = s.length();
-        int maxLength = 0;
-
-        for (int i = 0; i < n; i++) {  // Fix start index
-            boolean[] arr = new boolean[128];  // Track visited characters
-            int length = 0;
-
-            for (int j = i; j < n; j++) {  // Expand substring
-                char c = s.charAt(j);
-                if (arr[c]) break;  // Stop if duplicate is found
-                
-                arr[c] = true;  // Mark character as seen
-                length++;  
-                maxLength = Math.max(maxLength, length);
-            }
-        }
-        return maxLength;
+        int  n = s.length();
+        Map<Character, Integer> lastSeen = new HashMap<>();
+        int left = 0 , maxlen = 0;
         
-        */
+        for(int right = 0 ; right < n ; right++){
+            char ch = s.charAt(right);
+
+            if (lastSeen.containsKey(ch) && lastSeen.get(ch) >= left){
+                left = lastSeen.get(ch) + 1;
+            }
+
+            lastSeen.put(ch, right);
+            maxlen = Math.max(maxlen, right - left + 1);
+        }
+
+        return maxlen ;
     }
 }
