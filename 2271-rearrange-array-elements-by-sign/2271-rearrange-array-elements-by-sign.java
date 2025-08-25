0645c662381d4d16333b7188418aef0761c2a649
JANVI <cjanvi1010@gmail.com>
2025-08-25 21:17:44 +0530

    Time: 3 ms (100%), Space: 77.1 MB (49.14%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
@@ -1,1 +1,27 @@
-nums
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        int n = nums.length;
+        int pos = 0 , neg = 0 ;
+        int[] arr = new int[n];
+        int odd = 1 , even = 0 ;
+        
+
+       for(int i = 0 ; i < n ; i++){
+
+          if(nums[i] > 0){
+            arr[even] = nums[i];
+            even +=2;
+          }
+
+          if(nums[i] < 0){
+            arr[odd] = nums[i];
+            odd +=2;
+          }
+
+       }
+
+       return arr;
+
+
+    }
+}
