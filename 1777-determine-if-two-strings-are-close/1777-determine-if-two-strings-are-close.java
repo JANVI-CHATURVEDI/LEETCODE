bce441487dd868d4bb05b4805951308bcaedb780
JANVI <cjanvi1010@gmail.com>
2025-09-05 20:22:00 +0530

    Time: 66 ms (37.19%), Space: 46.1 MB (8.83%) - LeetHub

--- 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.java
+++ 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.java
@@ -1,52 +1,32 @@
 class Solution {
     public boolean closeStrings(String word1, String word2) {
-        int n1=word1.length(),n2=word2.length();
 
-        if(n1 != n2){
-            return false;
-        }
+        if (word1.length() != word2.length()) return false;
 
-        HashMap<Character,Integer> map1 = new HashMap<>();
-        HashMap<Character,Integer> map2 = new HashMap<>();
+        HashMap<Character, Integer> map1 = new HashMap<>();
+        HashMap<Character, Integer> map2 = new HashMap<>();
         
-         for(int i=0;i<n2;i++ ){
-            char ch =word1.charAt(i);
-          
-          map1.put(ch,map1.getOrDefault(ch, 0) + 1);
-           
+        int n1 = word1.length();
+        int n2 = word2.length();
+
+        for (char c : word1.toCharArray()) {
+        map1.put(c, map1.getOrDefault(c, 0) + 1);
         }
 
-         for(int i=0;i<n1;i++ ){
-             char ch =word2.charAt(i);
-          
-          map2.put(ch,map2.getOrDefault(ch, 0) + 1);
-           
+    
+        for (char c : word2.toCharArray()) {
+        map2.put(c, map2.getOrDefault(c, 0) + 1);
         }
 
-        
-         for(int i=0;i<n1;i++ ){
-             char ch =word1.charAt(i);
-          
-          if(map2.containsKey( ch )){
-            continue;
-          }
-          else{
-            return false;
-          }
-           
-        }
+        if (!map1.keySet().equals(map2.keySet())) return false;
 
         List<Integer> freq1 = new ArrayList<>(map1.values());
         List<Integer> freq2 = new ArrayList<>(map2.values());
-
         Collections.sort(freq1);
         Collections.sort(freq2);
 
-        if (!freq1.equals(freq2)) {
-                  return false;
-        }
+        return freq1.equals(freq2);
 
-
-        return true;
+        
     }
 }
