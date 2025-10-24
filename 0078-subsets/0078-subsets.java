a6febf5e6c2aad3b82be8f27f8310ec2aecbaad5
JANVI <cjanvi1010@gmail.com>
2025-10-24 21:33:12 +0530

    0078-SUBSETS Stats: Time: 0 ms (100%), Space: 42.8 MB (66.98%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,28 +1,25 @@
 class Solution {
-public void solve(int[] nums, List<Integer> output,int index,List<List<Integer>> ans){
-  
-  if(index>=nums.length){
-    ans.add(new ArrayList<>(output));
-    return;
-  }
-//exclude
-  solve(nums,output,index+1,ans);
-//include
-output.add(nums[index]);
-solve(nums,output,index+1,ans);
-
-//backtrack
-output.remove(output.size()-1);
-}
-
-
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> ans=new ArrayList<>();
-        List<Integer> output=new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        solve(0, nums, new ArrayList<>(), ans);
+        return ans;
+    }
+
+    void solve(int index, int[] nums, List<Integer> output, List<List<Integer>> ans) {
+       
+        if (index == nums.length) {
+            ans.add(new ArrayList<>(output));
+            return;
+        }
+
         
+        solve(index + 1, nums, output, ans);
 
-         solve(nums,output,0,ans);
-         return ans;
+        
+        output.add(nums[index]);
+        solve(index + 1, nums, output, ans);
 
+       
+        output.remove(output.size() - 1);
     }
 }
