efa3529499ebc2cb97f274afb28e51b143916114
JANVI <cjanvi1010@gmail.com>
2025-11-09 20:22:50 +0530

    0057-INSERT-INTERVAL Stats: Time: 3 ms (56.76%), Space: 14.2 MB (47.38%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def insert(self, intervals, newInterval):
+        """
+        :type intervals: List[List[int]]
+        :type newInterval: List[int]
+        :rtype: List[List[int]]
+        """
+        n = len(intervals)
+        merged = []
+        i = 0 
+
+        while i < n and intervals[i][1] < newInterval[0] :
+            merged.append(intervals[i])
+            i += 1
+
+        while i < n and intervals[i][0] <= newInterval[1] :
+            newInterval[0] = min(newInterval[0], intervals[i][0])
+            newInterval[1] = max(newInterval[1], intervals[i][1])
+            i += 1
+
+        merged.append(newInterval)    
+
+        while i < n :
+            merged.append(intervals[i])
+            i += 1   
+
+
+
+        return merged        
+
+
+
+
+        
+
