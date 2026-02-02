1da6a3624772918410c8722d3242b3acd015ab8c
JANVI <cjanvi1010@gmail.com>
2026-02-02 21:52:34 +0530

    0496-NEXT-GREATER-ELEMENT-I Stats: Time: 4 ms (41.06%), Space: 12.6 MB (58.07%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,20 +1,24 @@
 class Solution(object):
     def nextGreaterElement(self, nums1, nums2):
         stack = []
-        next_greater = {}
+        next_greater_map = {}
 
         for num in nums2:
             while stack and num > stack[-1]:
                 prev = stack.pop()
-                next_greater[prev] = num
+                next_greater_map[prev] = num
             stack.append(num)
 
-        while stack:
-            next_greater[stack.pop()] = -1
-
         
+        while stack :
+            prev = stack.pop()
+            next_greater_map[prev] = -1
+
         result = []
         for num in nums1:
-            result.append(next_greater[num])
+            result.append(next_greater_map[num])
 
-        return result
+        return result    
+                     
+
+
